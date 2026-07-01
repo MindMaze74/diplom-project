4c053ab25576615a02859198029574cffdb94c05
StarcevDA <isava2010@gmail.com>
2026-07-01 23:34:38 +0500

    сделаны правки

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,10 +4,13 @@ terraform {
     yandex = { source = "yandex-cloud/yandex", version = "~> 0.95.0" }
     time   = { source = "hashicorp/time", version = "~> 0.9.0" }
     local  = { source = "hashicorp/local", version = "~> 2.4.0" }
+    null   = { source = "hashicorp/null", version = "~> 3.2.0" }
   }
 }
 
-data "yandex_compute_image" "ubuntu" { family = "ubuntu-2204-lts" }
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2204-lts"
+}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
